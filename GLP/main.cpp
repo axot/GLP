e85906d386c7037359b8ad1507b3beff134ef51a
axot <axot@msn.com>
2012-12-20 20:05:46 +0900

    Initial Commit

---
+++ GLP/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GLP
+//
+//  Created by axot on 12/20/12.
+//  Copyright (c) 2012 Saigo Laboratoire. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
