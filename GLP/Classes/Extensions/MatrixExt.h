930fba8177fc6d928eaced5a812addc246af9292
Zheng Shao <axot@msn.com>
2014-04-02 00:56:36 +0900

    new fileload method

--- GLP/Classes/Extensions/MatrixExt.h
+++ GLP/Classes/Extensions/MatrixExt.h
@@ -26,6 +26,8 @@
 #ifndef EIGEN_MATRIXEXT_H
 #define EIGEN_MATRIXEXT_H
 
+#include <sys/param.h>
+#include <sys/mount.h>
 #include <iostream>
 #include <fcntl.h>
 #include <unistd.h>
@@ -39,18 +41,20 @@
 #include <Eigen/Sparse>
 #include <boost/xpressive/xpressive.hpp>
 
+#define SMALL_BUF_SIZE (1024 * 16)
+
 namespace EigenExt
 {
     using namespace std;
     using namespace Eigen;
     using namespace boost::xpressive;
-    
+
     typedef SparseMatrix<int> SMatrixXi;
 
     /* RegEx Version: flexible matrix format, but slower
-     * 
+     *
      * Example:
-     * 
+     *
      * MatrixXd X;
      * loadMatrixFromFile(X, "Matrix.data");
      */
@@ -63,7 +67,7 @@ namespace EigenExt
      * Matrix must be formated like below,
      * space can be replaced with any delimiter
      * and only signle delimiter is permitted
-     *   
+     *
      * Matrix Sample:
      * 1 2 3
      * 4 5 6
@@ -86,19 +90,22 @@ namespace EigenExt
     bool loadMatrixFromFileFast(MatrixType& m, const char* filename, const char delim, bool doesUseMemoryBoost = false);
 
     template<typename MatrixType>
+    bool loadMatrixFromFileSuperfast(MatrixType& m, const char* filename, const char delim);
+
+    template<typename MatrixType>
     MatrixType centering(MatrixType& m);
-    
+
     /* Implementation */
     inline void setMatValue(vector< Triplet<int> >& tripletList, MatrixXd& m, int row, int col, char* begin)
     {
         m(row,col) = atof(begin);
     }
-    
+
     inline void setMatValue(vector< Triplet<int> >& tripletList, MatrixXi& m, int row, int col, char* begin)
     {
         m(row,col) = atoi(begin);
     }
-        
+
     inline void setMatValue(vector< Triplet<int> >& tripletList, SMatrixXi& m, int row, int col, char* begin)
     {
         int real = atoi(begin);
@@ -113,13 +120,13 @@ namespace EigenExt
     {
         return;
     }
-    
+
     template<>
     inline void assignTripletList(vector< Triplet<int> >& tripletList, SMatrixXi& m)
     {
         m.setFromTriplets(tripletList.begin(), tripletList.end());
     }
-    
+
     template<typename MatrixType>
     bool loadMatrixFromFile(MatrixType& m, const char* filename)
     {
@@ -130,7 +137,7 @@ namespace EigenExt
         ssize_t read;
         int rows = 0;
         int cols = 0;
-    
+
         FILE *fp;
         fp = fopen(filename, "r");
         if (fp == NULL)
@@ -138,7 +145,7 @@ namespace EigenExt
             cerr << "open file failed: " << filename << endl;
             return false;
         }
-        
+
         // calc matrix size
         while ((read = getline(&line, &len, fp)) != -1)
         {
@@ -153,24 +160,24 @@ namespace EigenExt
             ++rows;
         }
         rewind(fp);
-        
+
         if (rows == 0 || cols == 0)
         {
             cerr << "resize matrix failed, rows: " << rows << " cols: " << cols << endl;
             return false;
         }
-        
+
         m.resize(rows, cols);
-        
+
         // fill matrix
         int currentRow = 0;
         int currentCol = 0;
-        
+
         while ((read = getline(&line, &len, fp)) != -1)
         {
             currentCol = 0;
             string str(line);
-            
+
             sregex_iterator cur(str.begin(), str.end(), regex);
             sregex_iterator end;
             for(; cur != end; ++cur)
@@ -181,18 +188,18 @@ namespace EigenExt
             }
             ++currentRow;
         }
-        
+
         free(line);
         fclose(fp);
         return true;
     }
-    
+
     template<typename MatrixType>
     bool loadMatrixFromFileFast(MatrixType& m, const char* filename, bool doesUseMemoryBoost)
     {
         return loadMatrixFromFileFast(m, filename, ' ', doesUseMemoryBoost);
     }
-    
+
     template<typename MatrixType>
     bool loadMatrixFromFileFast(MatrixType& m, const char* filename, const char delim, bool doesUseMemoryBoost)
     {
@@ -214,8 +221,8 @@ namespace EigenExt
                 cerr << "open file failed: " << filename << endl;
                 return false;
             }
-            
-            // calc matrix size            
+
+            // calc matrix size
             while ((read = getline(&line, &len, fp)) != -1)
             {
                 if (!rows)
@@ -232,20 +239,20 @@ namespace EigenExt
             }
             cols = delimCount + 1;
             rewind(fp);
-            
+
             if (rows == 0 || cols == 0)
             {
                 cerr << "resize matrix failed, rows: " << rows << " cols: " << cols << endl;
                 return false;
             }
-            
+
             m.resize(rows, cols);
-            
+
             // fill matrix
             int currentRow = 0;
             int currentCol = 0;
             size_t beginIndex = 0;
-            
+
             while ((read = getline(&line, &len, fp)) != -1)
             {
                 currentCol = 0;
@@ -266,7 +273,7 @@ namespace EigenExt
                 }
                 ++currentRow;
             }
-            
+
             free(line);
             fclose(fp);
         }
@@ -276,17 +283,17 @@ namespace EigenExt
             struct stat fs;
             size_t buf, buf_end;
             char *begin, *end;
-            
+
             if (fd == -1) {
                 cerr << "open file failed: " << filename << endl;
                 return false;
             }
-            
+
             if (fstat(fd, &fs) == -1) {
                 cerr << "stat file failed: " <<  filename << endl;
                 return false;
             }
-            
+
             /* fs.st_size could have been 0 actually */
             buf = (size_t)mmap(0, fs.st_size, PROT_READ, MAP_SHARED, fd, 0);
             if ((void*)buf == MAP_FAILED) {
@@ -294,7 +301,7 @@ namespace EigenExt
                 close(fd);
                 return false;
             }
-            
+
             buf_end = buf + fs.st_size;
             begin = (char *)buf;
             // calc matrix size
@@ -310,7 +317,7 @@ namespace EigenExt
                 if ( *begin == '\n' || *begin == '\r' )
                 {
                     ++rows;
-                    
+
                     /* see if we got "\r\n" or "\n\r" here */
                     if ( (size_t)begin+1 < buf_end && ( *(begin+1) == '\n' || *(begin+1) == '\r' ) )
                     {
@@ -319,21 +326,21 @@ namespace EigenExt
                 }
                 ++begin;
             }
-            
+
             cols = delimCount + 1;
-            
+
             if (rows == 0 || cols == 0)
             {
                 cerr << "resize matrix failed, rows: " << rows << " cols: " << cols << endl;
                 return false;
             }
-            
+
             m.resize(rows, cols);
-            
+
             // fill matrix
             int currentRow = 0;
             int currentCol = 0;
-            
+
             begin = (char *)buf;
             end = begin;
             while ( (size_t)end < buf_end )
@@ -362,11 +369,163 @@ namespace EigenExt
             munmap((void*)buf, fs.st_size);
             close(fd);
         }
-        
+
         assignTripletList(tripletList, m);
         return true;
     }
-    
+
+    template<typename MatrixType>
+    bool loadMatrixFromFileSuperfast(MatrixType& m, const char* filename, const char delim)
+    {
+        struct stat fs;
+#if defined(__bsdi__) || defined(__APPLE__)
+        struct statfs fsb;
+#endif
+        u_char small_buf[SMALL_BUF_SIZE];
+        u_char *buf = small_buf;
+        off_t buf_size = SMALL_BUF_SIZE;
+        u_char *p;
+
+        size_t delimCount = 0;
+        int rows = 0;
+        int cols = 0;
+        vector< Triplet<int> > tripletList;
+
+        int fd = open(filename, O_RDONLY);
+        if (fd == -1) {
+            cerr << "open file failed: " << filename << endl;
+            return false;
+        }
+
+        if (fstat(fd, &fs) == -1) {
+            cerr << "stat file failed: " <<  filename << endl;
+            return false;
+        }
+
+#ifdef __LINUX__
+        posix_fadvise(fd, 0, 0, 1);  // FDADVICE_SEQUENTIAL
+#endif
+
+#if defined(__bsdi__) || defined(__APPLE__)
+        if (fstatfs(fd, &fsb))
+        {
+            fsb.f_iosize = SMALL_BUF_SIZE;
+        }
+        if (fsb.f_iosize != buf_size)
+        {
+            if (buf != small_buf) {
+                free(buf);
+            }
+            if (fsb.f_iosize == SMALL_BUF_SIZE || !(buf = (u_char *)malloc(fsb.f_iosize))) {
+                buf = small_buf;
+                buf_size = SMALL_BUF_SIZE;
+            } else {
+                buf_size = fsb.f_iosize;
+            }
+        }
+#endif
+        // get matrix size
+        while (int len = read(fd, buf, buf_size))
+        {
+            if (len == -1)
+            {
+                cerr << "read file error: " << filename << endl;
+                close(fd);
+                return false;
+            }
+            for (p = buf; len--; ++p)
+            {
+                if (!rows)
+                    if (*p == delim) ++delimCount;
+
+                if (*p == '\n' || *p == '\r')
+                    ++rows;
+            }
+        }
+        cols = delimCount + 1;
+
+        if (rows == 0 || cols == 0)
+        {
+            cerr << "resize matrix failed, rows: " << rows << " cols: " << cols << endl;
+            close(fd);
+            return false;
+        }
+
+        // fill matrix
+        lseek(fd, 0, 0);
+        m.resize(rows, cols);
+
+        int currentRow = 0;
+        int currentCol = 0;
+        u_char *beginIndex = buf;
+
+        u_char mergedNumber[2*SMALL_BUF_SIZE];
+        long long numsize = 0;
+
+        while (int len = read(fd, buf, buf_size))
+        {
+            if (len == -1)
+            {
+                cerr << "read file error: " << filename << endl;
+                close(fd);
+                return false;
+            }
+            for (p = buf; len--; ++p)
+            {
+                if (*p == delim)
+                {
+                    if(numsize)
+                    {
+                        u_char *nump = (u_char *)((size_t)mergedNumber+numsize);
+                        memcpy(nump, beginIndex, (size_t)p - (size_t)beginIndex);
+                        *(nump+(size_t)p - (size_t)beginIndex) = 0;
+                        setMatValue(tripletList, m, currentRow, currentCol, (char*)&mergedNumber);
+                        numsize = 0;
+                    }
+                    else
+                        setMatValue(tripletList, m, currentRow, currentCol, (char*)beginIndex);
+
+                    ++currentCol;
+                    beginIndex = (u_char *)((size_t)p + 1);
+                }
+                else if (*p == '\n' || *p == '\r')
+                {
+                    if(numsize)
+                    {
+                        u_char *nump = (u_char *)((size_t)mergedNumber+numsize);
+                        memcpy(nump, beginIndex, (size_t)p - (size_t)beginIndex);
+                        *(nump+(size_t)p - (size_t)beginIndex) = 0;
+                        setMatValue(tripletList, m, currentRow, currentCol, (char*)&mergedNumber);
+                        numsize = 0;
+                    }
+                    else
+                        setMatValue(tripletList, m, currentRow, currentCol, (char*)beginIndex);
+
+                    currentCol = 0;
+                    beginIndex = (u_char *)((size_t)p + 1);
+                    ++currentRow;
+                }
+                if (!len)
+                {
+                    if(*p != '\n' && *p != '\r' && *p != delim)
+                    {
+                        numsize = (size_t)p - (size_t)beginIndex + 1;
+                        if (numsize)
+                            memcpy(mergedNumber, beginIndex, (size_t)numsize);
+                    }
+                    else
+                        numsize = 0;
+
+                    beginIndex = buf;
+                }
+            }
+        }
+
+        assignTripletList(tripletList, m);
+        close(fd);
+        return true;
+    }
+
     template<typename MatrixType>
     inline MatrixType centering(MatrixType& m)
     {
@@ -374,3 +533,4 @@ namespace EigenExt
     }
 };
 #endif
+
--- tools/dfs2sdf.pl
+++ tools/dfs2sdf.pl
@@ -1,9 +1,9 @@
-#!/usr/bin/perl 
+#!/usr/bin/env perl 
 # Author: Zheng Shao @2013.02.09
-# Version: 0.2
 # 
 # Change Log:
 #
+# 0.3: add charge support, for now only + and -. 20140128
 # 0.2: add --nobeta option, fix backward bug
 #
 # Input: dfs file
@@ -11,21 +11,42 @@
 
 use Getopt::Long;
 
+my $version = 0.3;
 my $nobeta = 0;
 
+sub printAtomLine
+{
+    my $atom = @_[0];
+    printf(OUT "    0.0000    0.0000    0.0000 ");
+
+    #0 = uncharged or value other than these, 1 = +3, 2 = +2, 3 = +1,
+    #4 = doublet radical, 5 = -1, 6 = -2, 7 = -3
+    if($hash{$atom} =~ /\+/){
+        $hash{$atom} =~ /(\S+)\+/;
+        printf(OUT "%-3s 0  3  0  0  0\n", $1);
+    }
+    elsif($hash{$atom} =~ /\-/){
+        $hash{$atom} =~ /(\S+)\-/;
+        printf(OUT "%-3s 0  5  0  0  0\n", $1);
+    }
+    else{
+        printf(OUT "%-3s 0  0  0  0  0\n", $hash{$atom});
+    }
+}
+
 if(@ARGV < 1){
-    die "usage: %perl [--nobeta] dfs2mol.pl model\n";
+    die "dfs2sdf version:$version by Zheng SHAO.\n\nusage:\nmkdir -p sdfs\n$0 [--nobeta] dfsfile\n";
 }
 
 GetOptions ('nobeta' => \$nobeta);
 
 $max = 1000; # maximum number of dfs patterns to process
 
-%hash = (0=>D, 1=>H, 2=>He, 3=>Li, 4=>Be, 5=>B, 6=>C, 7=>N, 8=>O, 9=>F, 10=>Ne, 11=>Na, 12=>Mg, 13=>Al, 14=>Si, 15=>P, 16=>S, 17=>Cl, Ar=>18, K=>19, Ca=>20, Sc=>21, Ti=>22, V=>23, Cr=>24, Mn=>25, Fe=>26, Co=>27, Ni=>28, Cu=>29, Zn=>30, Ga=>31, Ge=>32, As=>33, Se=>34, 35=>Br, Kr=>36, Rb=>37, Sr=>38, Y=>39, Zr=>40, Nb=>41, Mo=>42, Tc=>43, Ru=>44, Rh=>45, Pd=>46, Ag=>47, Cd=>48, In=>49, Sn=>50, Sb=>51, Te=>52, 53=>I, Xe=>54, Cs=>55, Ba=>56, La=>57, Ce=>58, Pr=>59, Nd=>60, Pm=>61, Sm=>62, Eu=>63, Gd=>64, Tb=>65, Ho=>67, Er=>68, Tm=>69, Yb=>70, Lu=>71, Hf=>72, Ta=>73, W=>74, Re=>75, Os=>76, Ir=>77, Pt=>78, Au=>79, Hg=>80, Tl=>81, Pb=>82, Bi=>83, Po=>84, At=>85, Rn=>86, Fr=>87, Ra=>88, Ac=>89, Th=>90, Pa=>91, U=>92, Np=>93, Pu=>94, Am=>95, Cm=>96, Bk=>97, Cf=>98, Es=>99, Fm=>100, Md=>101, No=>102, Lr=>103, Xx=>104);
+# Fixit: Md and H- has same atom number 101.
+%hash = (0=>D, 1=>H, 2=>He, 3=>Li, 4=>Be, 5=>B, 6=>C, 7=>N, 8=>O, 9=>F, 10=>Ne, 11=>Na, 12=>Mg, 13=>Al, 14=>Si, 15=>P, 16=>S, 17=>Cl, Ar=>18, K=>19, Ca=>20, Sc=>21, Ti=>22, V=>23, Cr=>24, Mn=>25, Fe=>26, Co=>27, Ni=>28, Cu=>29, Zn=>30, Ga=>31, Ge=>32, As=>33, Se=>34, 35=>Br, Kr=>36, Rb=>37, Sr=>38, Y=>39, Zr=>40, Nb=>41, Mo=>42, Tc=>43, Ru=>44, Rh=>45, Pd=>46, Ag=>47, Cd=>48, In=>49, Sn=>50, Sb=>51, Te=>52, 53=>I, Xe=>54, Cs=>55, Ba=>56, La=>57, Ce=>58, Pr=>59, Nd=>60, Pm=>61, Sm=>62, Eu=>63, Gd=>64, Tb=>65, Ho=>67, Er=>68, Tm=>69, Yb=>70, Lu=>71, Hf=>72, Ta=>73, W=>74, Re=>75, Os=>76, Ir=>77, Pt=>78, Au=>79, Hg=>80, Tl=>81, Pb=>82, Bi=>83, Po=>84, At=>85, Rn=>86, Fr=>87, Ra=>88, Ac=>89, Th=>90, Pa=>91, U=>92, Np=>93, Pu=>94, Am=>95, Cm=>96, Bk=>97, Cf=>98, Es=>99, Fm=>100, Md=>101, No=>102, Lr=>103, Xx=>104, 1001=>'H+', 101=>'H-', 1006=>'C+', 106=>'C-', 1007=>'N+', 107=>'N-', 1008=>'O+', 108=>'O-', 1009=>'F+', 109=>'F-', 1017=>'Cl+', 117=>'Cl-', 1015=>'P+', 115=>'P-', 1016=>'S+', 116=>'S-', 1035=>'Br+', 135=>'Br-', 1053=>'I+', 153=>'I-');
 
 %hash2 = (1=>"solid", 2=>"dotted", 3=>"bald", 4=>"dashed"); 
 %hash2 = (1=>"black", 2=>"orange", 3=>"red", 4=>"green"); 
-
 
 while(<>){
     open(OUT,">sdfs/$.\.mol");
@@ -59,13 +80,12 @@ while(<>){
     }
     printf(OUT "%3d%3d  0  0  0  0  0  0  0  0  1 V2000\n",$atomnum,$edgenum);
     $first[0] =~ /\((\d+)\)/;
-    printf(OUT "    0.0000    0.0000    0.0000 %-3s 0  0  0  0  0\n",$hash{$1});
+    printAtomLine($1);
 
     foreach $pair (@bondatom){
         if($pair =~ /f/){ #forward
             $pair =~ /(\d+)\s\((\d+)f(\d+)\)/;
-            $atom_type = $3;
-            printf(OUT "    0.0000    0.0000    0.0000 %-3s 0  0  0  0  0\n",$hash{$atom_type});
+            printAtomLine($3);
         }
     }
 
